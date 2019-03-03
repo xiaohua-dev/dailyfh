930b5bb1102ea723a46e806bbcea05f0740fc84c
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-03-03 20:33:25 +0800

    030301

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 #from user import views
 from user.views import RegisterView, ActiveView, LoginView,UserInfoView,UserOrderView,UserSiteView
 
@@ -8,7 +9,7 @@ urlpatterns = [
     url(r'^register$', RegisterView.as_view(), name='register'),
     url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),
     url(r'^login$', LoginView.as_view(), name='login'),
-    url(r'^$', UserInfoView.as_view(), name='user'),
-    url(r'^order$', UserOrderView.as_view(), name='order'),
-    url(r'^address$', UserSiteView.as_view(), name='address'),
+    url(r'^$',login_required( UserInfoView.as_view()), name='user'),
+    url(r'^order$', login_required(UserOrderView.as_view()), name='order'),
+    url(r'^address$', login_required(UserSiteView.as_view()), name='address'),
 ]
